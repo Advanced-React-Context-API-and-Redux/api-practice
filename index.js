2c3db6396d7203ecb2fc570aadb8279b7e1c5d34
seanaleid <seanaleid@gmail.com>
2020-03-11 21:51:08 -0400

    Initialize repo
      * Adds package.json, server.js, index.js and functional code

---
+++ index.js
@@ -0,0 +1,8 @@
+const server = require('./server.js');
+const PORT = 8000
+
+server.listen(PORT, () => {
+    console.log(`\n *** Your server is running on port ${PORT}. Let's get coding!! 🏄🏄‍♀️🏄‍♂️🌊🌊🌊`)
+})
+
+/* Go to https://www.youtube.com/watch?v=dQw4w9WgXcQ and follow the instructions and code! */
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+// importing helmet
+const helmet = require('helmet');
+
+// import routers
+
+
+// creates the server instance
+const server = express();
+
+// allowing express to parse json
+server.use(server.json());
+server.use(helmet());
+
+// initial route to check server
+server.get('/', (req, res) => {
+    res.status(200).json({ message: "The server is running 🤙!!"});
+});
+
+// set the routers
+// tells the server to use these routers
+
+
+module.exports = server;
